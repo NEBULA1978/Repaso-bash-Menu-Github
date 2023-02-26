b794b1410442ac41ba41c748240a3faa39994d87
NEBULA1978 <rpascualey@gmail.com>
2023-02-26 17:02:34 +0100

    Añadiendo copiar lineas al archivo./menubase4-completo-while-case.sh

--- menubase4-completo-while-case.sh
+++ menubase4-completo-while-case.sh
@@ -24,6 +24,27 @@ function show_content {
       if [ -f "$dir/$file" ]; then
         cat "$dir/$file"
         read -p "Presiona enter para continuar."
+        ls -l
+        echo "Introduce el nombre del archivo:"
+        read archivo
+        echo
+        printf " 1  2  3\n"
+        batcat $archivo
+        echo "Desde qué línea desea copiar?"
+        read inicio
+        echo "Hasta qué línea desea copiar?"
+        read fin
+
+        # Añadir un contador al nombre de archivo resultado
+        counter=0
+        while [ -e "resultado$counter.txt" ]; do
+          counter=$((counter + 1))
+        done
+
+        # Guardar las líneas seleccionadas en un archivo resultado con el contador
+        sed -n "${inicio},${fin}p" "$archivo" >>resultado$counter.txt
+        echo
+        echo "Las líneas seleccionadas se han guardado en resultado$counter.txt"
       else
         echo "El archivo $file no existe."
       fi
